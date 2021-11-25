b20265b58ec3dabd0ecda38f1c32fd65f036d1c5
Bailey Lievens <baileylievens@hotmail.be>
2021-11-25 18:05:48 +0100

    F: added semi working cookies

--- js/GTM.js
+++ js/GTM.js
@@ -1,4 +1,16 @@
+let reload = false;
+let _trackingCookies = [];
+
 function initGTM(w, d, s, l, i) {
+    console.log("reload = " + reload);
+    console.log("tracking = " + _trackingCookies);
+    if(reload){
+        for (let index = 0; index < _trackingCookies.length; index++) {
+            document.cookie = _trackingCookies[index].name + "; ";
+            console.log(_trackingCookies[index]);
+        }
+    }
+    reload = true;
     w[l] = w[l] || [];
     w[l].push({
         'gtm.start': new Date().getTime(),
@@ -11,4 +23,14 @@ function initGTM(w, d, s, l, i) {
     j.src =
         'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
     f.parentNode.insertBefore(j, f);
+}
+
+function stopGTM() {
+    cookieStore.getAll().then(cookies => cookies.forEach(cookie => {
+        if(cookie.name == "cookieconsent_status"){
+            return;
+        }
+        _trackingCookies.push(cookie.name, cookie.value);
+        cookieStore.delete(cookie.name);
+    }));
 }
