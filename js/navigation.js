d8862d0d35a2ba98eba4e07faf45a67147e72d8d
bailey lievens <baileylievens@hotmail.be>
2021-12-26 20:34:30 +0100

    Fix: fixed some feedback points

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,6 @@
 let hamburger = document.querySelector(".hamburger");
 let compactMenu = document.querySelector(".menu-wrap-compact nav");
 let fullMenubg = document.querySelector(".menu-wrap-full");
-let fullMenubgBeta = document.querySelector(".menu-wrap-full-beta");
 let compactMenubg = document.querySelector(".menu-wrap-compact");
 
 let scrollOffsetMinimum = 50;
@@ -28,13 +27,11 @@ window.addEventListener("scroll", function(){
     let scrollOffset = window.scrollY;
     
     if(scrollOffset > scrollOffsetMinimum) {
-        // fullMenubg.style.backgroundColor = "var(--bgColor)";
-        fullMenubgBeta.style.backgroundColor = "var(--bgColor)";
+        fullMenubg.style.backgroundColor = "var(--bgColor)";
         compactMenubg.style.backgroundColor = "var(--bgColor)";
         return;
     }
 
-    // fullMenubg.style.backgroundColor = "transparent";
-    fullMenubgBeta.style.backgroundColor = "transparent";
+    fullMenubg.style.backgroundColor = "transparent";
     compactMenubg.style.backgroundColor = "transparent";
 });
---
+++ js/titleTimer.js
@@ -0,0 +1,16 @@
+var introKeywords = ["website", "branding", "agency", "app", "illustration"];
+var currentKeywordId = 0;
+var timerDuration = .9;
+
+var keywordLocation = document.getElementById("h1_KeyActivity");
+
+var intervalTitle = setInterval(changeTitle, timerDuration * 1000);
+
+function changeTitle() {
+    keywordLocation.innerHTML = introKeywords[currentKeywordId];
+    
+    currentKeywordId++;
+    if(currentKeywordId > introKeywords.length - 1) {
+        currentKeywordId = 0;
+    }
+}
